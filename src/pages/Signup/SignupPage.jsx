ad727377ed2e5fa547f4bacd76caaade7928eea2
yerin <monicayerin40@gmail.com>
2025-08-11 23:37:51 -0700

    Revert "Feature/header component"

--- src/pages/Signup/SignupPage.jsx
+++
@@ -1,3 +0,0 @@
-export default function SignupPage() {
-  return <h1>회원가입 페이지</h1>;
-}
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "../pages/Home/HomePage";
 import LoginPage from "../pages/Login/LoginPage";
 import SignupPage from "../pages/Signup/SignupPage";
@@ -7,16 +7,18 @@ import MyPage from "../pages/MyPage";
 import NotFound from "../pages/NotFound";
 
 const AppRouter = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/main" element={<MainPage />} />
-      <Route path="/mypage" element={<MyPage />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/signup" element={<SignupPage />} />
+                <Route path="/main" element={<MainPage />} />
+                <Route path="/mypage" element={<MyPage />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default AppRouter;
