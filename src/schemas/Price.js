972add060a28fa5dc2cee5879c7a9af8323d5272
soyo <164558822+oyossss@users.noreply.github.com>
2025-08-16 22:46:29 +0900

    Create Price.js

---
+++ src/schemas/Price.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    id: 'deposit',
+    label: '보증금 (만원)',
+    type: 'range',
+    min: 0,
+    max: 5000,
+    step: 50
+  },
+  {
+    id: 'monthlyRent',
+    label: '월세 (만원)',
+    type: 'range',
+    min: 0,
+    max: 200,
+    step: 5
+  }
+];
