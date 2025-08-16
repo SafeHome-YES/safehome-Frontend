f4a2653e322b09cef5b3a4fff0fa66ab4454aef4
soyo <164558822+oyossss@users.noreply.github.com>
2025-08-16 22:45:31 +0900

    Create index.js

---
+++ src/schemas/index.js
@@ -0,0 +1,14 @@
+// src/schemas/index.js
+import region from './region.js'; 
+import price from './price.js';
+import infra from './infra.js';
+import listing from './listing.js';
+import safety from './safety.js';
+
+export const SCHEMAS = {
+  region,
+  price,
+  infra,
+  listing,
+  safety,
+};
